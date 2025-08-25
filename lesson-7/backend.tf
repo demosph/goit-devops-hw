b2e56b60c8cd245c88e35a01e6ea386c2bcad6e8
eugene.timoshenko <da_etimoshenko@phreesia.com>
2025-08-25 19:37:25 +0300

    Added Terraform EKS module and django-app Helm chart.

---
+++ lesson-7/backend.tf
@@ -0,0 +1,13 @@
+ terraform {
+  backend "s3" {
+    bucket         = "goit-tfstate-bucket"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+ }
+
+provider "aws" {
+  region = "us-east-2"
+}
