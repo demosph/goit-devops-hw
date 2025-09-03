f26e035b80bb095a11df2930feb6b1d027b68404
eugene.timoshenko <da_etimoshenko@phreesia.com>
2025-09-03 11:13:27 +0300

    Removed secretes

--- lesson-db-module/variables.tf
+++ lesson-db-module/variables.tf
@@ -6,12 +6,10 @@ variable "region" {
 variable "github_user" {
   description = "GitHub username"
   type        = string
-  default     = "demosph"
 }
 
 variable "github_pat" {
   description = "GitHub Personal Access Token"
   type        = string
   sensitive   = true
-  default     = "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"
 }
